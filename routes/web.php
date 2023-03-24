d2e371252650a07925bf67eb062a14e7da405f00
Davidlt4 <lopez21tap@gmail.com>
2023-03-24 13:52:03 +0100

    Guardado del viernes

--- routes/web.php
+++ routes/web.php
@@ -31,8 +31,6 @@ Route::prefix('/')->group(function(){
 
     //para enviar correo
 
-    Route::get('/enviar',[Controller::class,'enviarCorreo']);
-
     Route::post('/enviar',[CorreoController::class,'enviarCorreo'])->name('enviar.correo');
 
     Route::get('/enviar',[CorreoController::class,'enviarCorreo'])->name('enviar.correo');
